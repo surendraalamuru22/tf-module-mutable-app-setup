d29cda96b0b183e1836ce23b1ecf7d0b53d6c968
surendra <surendraredddyalamuru01@gmail.com>
2024-04-22 16:50:55 +0530

    apps

--- asg.tf
+++ asg.tf
@@ -9,11 +9,17 @@ resource "aws_autoscaling_group" "asg" {
   desired_capacity    = var.min_size
   max_size            = var.max_size
   min_size            = var.min_size
-#  vpc_zone_identifier = var.subnets
+  vpc_zone_identifier = var.subnets
 #  target_group_arns   = [aws_lb_target_group.main.arn]
 
   launch_template {
     id      = aws_launch_template.launch-template.id
     version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "${var.env}-${var.name}"
+    propagate_at_launch = true
   }
 }
