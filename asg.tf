93beaeb9eadd236d97522ba265b1698186a07901
surendra <surendraredddyalamuru01@gmail.com>
2024-04-22 16:41:57 +0530

    apps

---
+++ asg.tf
@@ -0,0 +1,19 @@
+resource "aws_launch_template" "launch-template" {
+  name          = "${var.env}-${var.name}-lt"
+  image_id      = data.aws_ami.centos-8-ami.image_id
+  instance_type = var.instance_type
+}
+
+resource "aws_autoscaling_group" "asg" {
+  name                = "${var.env}-${var.name}-asg"
+  desired_capacity    = var.min_size
+  max_size            = var.max_size
+  min_size            = var.min_size
+#  vpc_zone_identifier = var.subnets
+#  target_group_arns   = [aws_lb_target_group.main.arn]
+
+  launch_template {
+    id      = aws_launch_template.launch-template.id
+    version = "$Latest"
+  }
+}
